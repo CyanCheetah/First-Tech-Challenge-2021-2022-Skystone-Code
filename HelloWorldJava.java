778608864d6680eca99bd280e80f0cfa1c49905a
CyanCheetah <91763642+CyanCheetah@users.noreply.github.com>
2022-02-12 12:15:00 -0600

    Delete HelloWorldJava.java

--- HelloWorldJava.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorldJava {
-    public static void main(String[] args) {
-        String hello = "Hello World !";
-        int Tanuj = 854388;
-        System.out.println(hello);
-        System.out.println("CyanCheetah");
-    }
-}
